fd277a52338bef01a99d33afa2b5d6149233c467
Jakub Łokieć <jakublokiec@wp.pl>
2019-06-05 23:09:13 +0200

    Create simple CLI app for testing purposes

---
+++ PKCK-zadanie-5/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XmlService;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xmlReader = new MusicXmlReader();
+            DataContext dataContext = xmlReader.Read(@"..\..\..\..\resources\music.xml");
+
+            // list musicians
+            foreach (var m in dataContext.Cds)
+            {
+                Console.WriteLine(m);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
