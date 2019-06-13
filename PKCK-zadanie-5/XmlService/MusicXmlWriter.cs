106a85ab682065bd77e173757468431545c7cebc
Jakub Łokieć <jakublokiec@wp.pl>
2019-06-13 17:38:27 +0200

    Replace var with type name in MusicXmlWriter

--- PKCK-zadanie-5/XmlService/MusicXmlWriter.cs
+++ PKCK-zadanie-5/XmlService/MusicXmlWriter.cs
@@ -12,7 +12,7 @@ namespace XmlService
 
         public static void Serialize(DataContext dataContext, string path)
         {
-            var header = new XElement("header", new XElement("authors",
+            XElement header = new XElement("header", new XElement("authors",
             new XElement[]
             {
                     new XElement("author",
@@ -28,32 +28,32 @@ namespace XmlService
             new XElement("modificationDate", "2019-06-13")
             );
 
-            var musicians = new XElement("musicians", dataContext.Musicians.Musicians.Select(d => new XElement("musician",
+            XElement musicians = new XElement("musicians", dataContext.Musicians.Musicians.Select(d => new XElement("musician",
                new XAttribute("musicianId", d.Value.Id),
                new XElement("name", d.Value.Name))).ToArray());
 
-            var genres = new XElement("genres", dataContext.Genres.Genres.Select(d => new XElement("genre",
+            XElement genres = new XElement("genres", dataContext.Genres.Genres.Select(d => new XElement("genre",
              new XAttribute("genreId", d.Value.Id),
              new XAttribute("genreName", d.Value.Name))).ToArray());
 
-            var languages = new XElement("languages", dataContext.Languages.Languages.Select(d => new XElement("language",
+            XElement languages = new XElement("languages", dataContext.Languages.Languages.Select(d => new XElement("language",
             new XAttribute("languageId", d.Value.Id), d.Value.Name)).ToArray());
 
-            var distributors = new XElement("distributors", dataContext.Distributors.Distributors.Select(d => new XElement("distributor",
+            XElement distributors = new XElement("distributors", dataContext.Distributors.Distributors.Select(d => new XElement("distributor",
             new XAttribute("distributorId", d.Value.Id), d.Value.Name)).ToArray());
 
-            var catalog = new XElement("catalog", dataContext.Cds.Select(d => new XElement("cd", 
+            XElement catalog = new XElement("catalog", dataContext.Cds.Select(d => new XElement("cd",
             new XElement("cdMusician", new XAttribute("musicianId", d.Musician.Id)),
             new XElement("title", d.Title),
             new XElement("cdGenre", new XAttribute("genreId", d.Genre.Id)),
             new XElement("cdLanguage", new XAttribute("languageId", d.Language.Id)),
             new XElement("premiereDate", d.PremiereDate),
-            new XElement("numberOfCds", d.NumberOfCds), 
+            new XElement("numberOfCds", d.NumberOfCds),
             new XElement("price", new XAttribute("currency", d.Price.Currency), d.Price.Value),
             new XElement("cdDistributor", new XAttribute("distributorId", d.Distributor.Id)))).ToArray());
 
 
-            var root = new XElement("document", header, musicians, genres, languages, distributors, catalog);
+            XElement root = new XElement("document", header, musicians, genres, languages, distributors, catalog);
             XDocument doc = new XDocument(new XDeclaration("1.0", "UTF-8", null), root);
 
             doc.Save(path);
