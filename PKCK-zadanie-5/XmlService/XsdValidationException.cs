b2f16d4ab2b2c3985d8b9c8e1abf9a36a8d5db42
Jakub Łokieć <jakublokiec@wp.pl>
2019-06-13 19:40:22 +0200

    Create exception for XSD validation

---
+++ PKCK-zadanie-5/XmlService/XsdValidationException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace XmlService
+{
+    public class XsdValidationException : Exception
+    {
+        public XsdValidationException()
+        {
+        }
+
+        public XsdValidationException(string message) : base(message)
+        {
+        }
+
+        public XsdValidationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected XsdValidationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
