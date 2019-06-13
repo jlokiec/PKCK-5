bd40dde5b9d828e019efd286d036fc2ac98ccb78
Jakub Łokieć <jakublokiec@wp.pl>
2019-06-13 18:38:27 +0200

    Change PremiereDate format to dd.MM.yyyy

--- PKCK-zadanie-5/PKCK-zadanie-5/ViewModel/AddViewModel.cs
+++ PKCK-zadanie-5/PKCK-zadanie-5/ViewModel/AddViewModel.cs
@@ -1,12 +1,7 @@
 ﻿using DataModel;
-using PKCK_zadanie_5.View;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using XmlService;
 
@@ -16,41 +11,39 @@ namespace PKCK_zadanie_5.ViewModel
     {
         public event PropertyChangedEventHandler PropertyChanged;
         public DataContext DataContext { get; set; }
-        public String Title { get; set; }
-        public String Musician { get; set; }
-        public String Language { get; set; }
-        public String Distributor { get; set; }
-        public String Genre { get; set; }
+        public string Title { get; set; }
+        public string Musician { get; set; }
+        public string Language { get; set; }
+        public string Distributor { get; set; }
+        public string Genre { get; set; }
         public double Price { get; set; }
         public int NumberOfCds { get; set; }
-        public String PremiereDate { get; set; }
-        public String Currency { get; set; }
+        public DateTime PremiereDate { get; set; }
+        public string Currency { get; set; }
         public ICommand AddCommand { get; set; }
-        MusicXmlReader xmlReader = new MusicXmlReader();
         public ObservableCollection<Cd> Cds { get; set; }
         public AddViewModel(ObservableCollection<Cd> cds, DataContext dataContext)
         {
+            PremiereDate = DateTime.Today;
             Cds = cds;
             AddCommand = new RelayCommand(pars => Add());
             DataContext = dataContext;
         }
         public void Add()
         {
-            Musician musician = new Musician(Musician.Substring(0,2), Musician);
+            Musician musician = new Musician(Musician.Substring(0, 4).ToUpper(), Musician);
             DataContext.Musicians.Add(musician);
-            Distributor distributor = new Distributor(Distributor.Substring(0,2), Distributor);
+            Distributor distributor = new Distributor(Distributor.Substring(0, 4).ToUpper(), Distributor);
             DataContext.Distributors.Add(distributor);
-            Genre genre = new Genre(Genre.Substring(0,2), Genre);
+            Genre genre = new Genre(Genre.Substring(0, 4).ToUpper(), Genre);
             DataContext.Genres.Add(genre);
-            Language language = new Language(Language.Substring(0,2), Language);
+            Language language = new Language(Language, Language);
             DataContext.Languages.Add(language);
             Price price = new Price(Price, Currency);
-        
-            Cd cd = new Cd(musician, Title, genre, language, PremiereDate.ToString(), NumberOfCds, price, distributor);
+
+            Cd cd = new Cd(musician, Title, genre, language, PremiereDate.ToString("dd.MM.yyyy"), NumberOfCds, price, distributor);
             DataContext.Cds.Add(cd);
             Cds.Add(cd);
-        
-           
         }
     }
 }
